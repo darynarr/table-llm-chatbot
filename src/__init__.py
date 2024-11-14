c990ca5fac63bc93a56f55e3d24bdc621f2d5d44
Daryna Ronska <35700510+darynarr@users.noreply.github.com>
2024-11-14 21:20:45 +0200

    Baseline

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+from src.database import Database
+from src.sql_agent import SQLAgent
+
+__all__ = ["Database", "SQLAgent"]
---
+++ src/database.py
@@ -0,0 +1,34 @@
+from math import e
+import os
+import pandas as pd
+from langchain_community.utilities.sql_database import SQLDatabase
+from sqlalchemy import create_engine
+
+class Database:
+    def __init__(self, db_name: str = 'database'):
+        self.engine = create_engine(f"sqlite:///{db_name}.db")
+        
+    def dataframe_to_table(self, df: pd.DataFrame, table_name: str):
+        df.to_sql(table_name, self.engine, index=True, if_exists='replace')
+    
+    def read(self, path: str):
+        filename = path.name
+        if filename.endswith('.csv'):
+            df = pd.read_csv(path)
+        elif filename.endswith('.xlsx') or filename.endswith('.xls'):
+            df = pd.read_excel(path)
+        elif filename.endswith('.parquet'):
+            df = pd.read_parquet(path)
+        else:
+            raise ValueError("Unsupported file format")
+        table_name = filename.split('.')[0]
+        self.dataframe_to_table(df, table_name)
+        
+    def get_sql_database(self, ):
+        return SQLDatabase(engine=self.engine)
+
+    def drop_database(self, db_name: str = 'database'):
+        try:
+            os.remove(f"{db_name}.db")
+        except FileNotFoundError:
+            print(f"{db_name}.db not found")
